7f5482ec16c024e3177c1298a575c76bc4ea8d77
ivar <ivar.grimstad@cybercom.com>
2014-02-10 10:56:14 +0100

    fixed scopes

--- javaee7/src/main/java/eu/agilejava/javaee7/GreetingCounter.java
+++ javaee7/src/main/java/eu/agilejava/javaee7/GreetingCounter.java
@@ -1,13 +1,13 @@
 package eu.agilejava.javaee7;
 
 import java.util.concurrent.atomic.AtomicLong;
-import javax.enterprise.context.Dependent;
+import javax.inject.Singleton;
 
 /**
  *
  * @author Ivar Grimstad (ivar.grimstad@gmail.com)
  */
-@Dependent
+@Singleton
 public class GreetingCounter {
 
    private final AtomicLong counter = new AtomicLong();
