ce0b11e84df9d64046ade0b744df471bb1055bc1
Ivar Grimstad <ivar.grimstad@gmail.com>
2014-07-03 19:57:20 +0200

    added simple spring counter

--- javaee7/src/main/java/eu/agilejava/javaee7/GreetingController.java
+++ javaee7/src/main/java/eu/agilejava/javaee7/GreetingController.java
@@ -3,23 +3,28 @@
  *
  * Copyright 2014 Ivar Grimstad <ivar.grimstad@gmail.com>.
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
- * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
- * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
- * Software.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
- * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
- * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 package eu.agilejava.javaee7;
 
+import eu.agilejava.spring4.SimpleSpringCounter;
 import javax.inject.Inject;
-import javax.inject.Singleton;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -32,18 +37,23 @@ import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
  *
  * @author Ivar Grimstad <ivar.grimstad@gmail.com>
  */
-@Singleton
 @Path("greeting")
 public class GreetingController {
 
-   @Inject
-   private GreetingCounter counter;
-   private static final String TEMPLATE = "Hello, %s";
+    private static final String TEMPLATE = "Hello, %s";
 
-   @GET
-   @Produces(APPLICATION_JSON)
-   public Greeting greet(@QueryParam("name") @DefaultValue("World") String name) {
+    @Inject
+    private GreetingCounter counter;
 
-      return new Greeting(counter.next(), String.format(TEMPLATE, name));
-   }
+    @Inject
+    private SimpleSpringCounter simpleSpringBean;
+
+    @GET
+    @Produces(APPLICATION_JSON)
+    public Greeting greet(@QueryParam("name") @DefaultValue("World") String name) {
+
+//      return new Greeting(counter.next(), String.format(TEMPLATE, name));
+        return new Greeting(simpleSpringBean.simpleCount(), String.format(TEMPLATE, name));
+
+    }
 }
