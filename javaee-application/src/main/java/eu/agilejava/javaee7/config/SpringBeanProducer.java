440211305a13b29dd8e243e6bdae749c794fd1d5
Ivar Grimstad <ivar.grimstad@gmail.com>
2014-10-12 15:42:32 +0200

    Java EE Application: refactoring producers
    One producer for both beans

--- javaee-application/src/main/java/eu/agilejava/javaee7/config/SpringBeanProducer.java
+++ javaee-application/src/main/java/eu/agilejava/javaee7/config/SpringBeanProducer.java
@@ -25,6 +25,7 @@ package eu.agilejava.javaee7.config;
 
 import eu.agilejava.spring4.SpringApplicationConfig;
 import eu.agilejava.spring4.AwsomeSpringCounter;
+import eu.agilejava.spring4.SimpleSpringCounter;
 import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Produces;
@@ -36,24 +37,37 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  * @author ivar.grimstad <ivar.grimstad@gmail.com>
  */
 @Singleton
-public class AwsomeSpringBeanProducer {
+public class SpringBeanProducer {
 
    private static final Logger LOGGER = Logger.getLogger("Java EE 7 Application");
 
+   private final SimpleSpringCounter simpleSpringCounter = new SimpleSpringCounter();
    private AnnotationConfigApplicationContext ctx;
-   
+
    /**
-    * Creates an instance of AwsomeSpringBeanProducer.
+    * Creates an instance of SpringBeanProducer.
     */
-   public AwsomeSpringBeanProducer() {
+   public SpringBeanProducer() {
       LOGGER.fine(() -> this.getClass().getSimpleName() + " created");
    }
 
-   /**
-    * Produces an AwsomeSpringCounter.
-    * Retrieves the AwsomeSpringCounterBean from spring context.
+    /**
+    * Produces a SimpleSpringCounter.
     * 
-    * @return The Spring Bean instance of AwsomeSpringCounter 
+    * @return The an instance of SimpleSpringCounter managed by this app's context.
+    */
+   @Produces
+   public SimpleSpringCounter simpleCounter() {
+      
+      LOGGER.info(() -> this.getClass().getSimpleName() + " producing simpleCounter");
+      return simpleSpringCounter;
+   }
+   
+   /**
+    * Produces an AwsomeSpringCounter. 
+    * Retrieves the AwsomeSpringCounterBean from spring context.
+    *
+    * @return The Spring Bean instance of AwsomeSpringCounter
     */
    @Produces
    public AwsomeSpringCounter awsomeCounter() {
@@ -61,15 +75,6 @@ public class AwsomeSpringBeanProducer {
       LOGGER.info(() -> this.getClass().getSimpleName() + " producing awsomeCounter");
       return ctx.getBean(AwsomeSpringCounter.class);
    }
-
-/**
-   @Produces
-   public SimpleSpringCounter simpleCounter() {
-
-      LOGGER.info(() -> this.getClass().getSimpleName() + " producing simpleCounter");
-      return ctx.getBean(SimpleSpringCounter.class);
-   }
-*/
 
    @PostConstruct
    private void init() {
